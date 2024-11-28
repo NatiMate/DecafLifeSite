f37e15f8f0bc8757e1c8c1ceb87001105d0f6cf7
MeintrupStefan <72495543+MeintrupStefan@users.noreply.github.com>
2024-11-28 03:07:11 +0100

    Adding favicon & seo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,16 @@
 	import Cta from './components/Cta.svelte';
 	import Headline from './components/Headline.svelte';
 	import ScreenPreviews from './components/ScreenPreviews.svelte';
+	import Seo from './components/Seo.svelte';
 	import Testimonials from './components/Testimonials.svelte';
 </script>
+
+<Seo
+	title="My Decaf Life"
+	image="https://my-decaf-life.com/favicon.webp"
+	description="Quit caffeine for a better life."
+	type="Website"
+/>
 
 <Headline />
 <Benefits />
---
+++ src/routes/components/Seo.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { page } from '$app/stores';
+
+	export let title;
+	export let description;
+	export let type = 'Website';
+	export let image;
+
+	let url = $page.url.href;
+</script>
+
+<svelte:head>
+	<title>Rezepte, die im Angebot sind</title>
+	<meta name="title" content={title} />
+	<meta name="description" content={description} />
+	<meta name="robots" content="index, follow" />
+	<meta
+		name="keywords"
+		content="günstige, rezepte, angebote, im Angebot, günstig, einfach, lecker, kochen, kochrezepte, kochideen, essen, rabatt, sparen, supermarkt"
+	/>
+	<meta name="image" content="" />
+	{@html `<script type="application/ld+json">
+		{
+			"@context": "https://schema.org",
+			"@type": "${type}",
+			"name": "${title}",
+            "image": "${image}",
+			"description": "${description}",
+			"url": "${url}"
+		}
+</script>`}
+</svelte:head>

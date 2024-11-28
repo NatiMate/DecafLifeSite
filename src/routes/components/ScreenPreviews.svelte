d09efc363b4c009a043de3428d77ae913028ba24
MeintrupStefan <72495543+MeintrupStefan@users.noreply.github.com>
2024-11-28 01:40:53 +0100

    Alternate images on desktop

--- src/routes/components/ScreenPreviews.svelte
+++ src/routes/components/ScreenPreviews.svelte
@@ -39,19 +39,39 @@
 	];
 </script>
 
-{#snippet preview(p: ScreenPreview)}
-	<div class="mx-auto flex flex-col gap-8 lg:flex-row lg:gap-20">
-		<img src={p.image} alt="Preview" class="hidden max-w-[500px] lg:block" />
-		<div class="m-auto flex max-w-md flex-col gap-2 lg:gap-8">
+{#snippet preview(p: ScreenPreview, i: number)}
+	<!-- Desktop version -->
+	<div class="mx-auto hidden flex-row gap-20 lg:flex" class:flex-row-reverse={i % 2 === 1}>
+		<img src={p.image} alt="Preview" class="max-w-[500px]" />
+		<div class="m-auto flex max-w-md flex-col gap-8">
+			<!-- Headline -->
 			<div class="gap-2 text-center">
 				<h2 class="leading-tight lg:text-left lg:text-5xl">
 					<span>{p.titleBlack1}</span> <span class="text-secondary-500">{p.titleGreen}</span>
 					<span>{p.titleBlack2}</span>
 				</h2>
 			</div>
-			<img src={p.image} alt="Preview" class="block lg:hidden" />
 			<h3
-				class="text-text-400 mb-8 text-center text-base font-normal lg:mb-0 lg:text-left lg:text-xl"
+				class="mb-8 text-center text-base font-normal text-text-400 lg:mb-0 lg:text-left lg:text-xl"
+			>
+				{p.description}
+			</h3>
+		</div>
+	</div>
+
+	<!-- Mobile version -->
+	<div class="mx-auto flex flex-col gap-8 lg:hidden lg:flex-row lg:gap-20">
+		<div class="m-auto flex max-w-md flex-col gap-2">
+			<div class="gap-2 text-center">
+				<!-- Headline -->
+				<h2 class="leading-tight lg:text-left lg:text-5xl">
+					<span>{p.titleBlack1}</span> <span class="text-secondary-500">{p.titleGreen}</span>
+					<span>{p.titleBlack2}</span>
+				</h2>
+			</div>
+			<img src={p.image} alt="Preview" />
+			<h3
+				class="mb-8 text-center text-base font-normal text-text-400 lg:mb-0 lg:text-left lg:text-xl"
 			>
 				{p.description}
 			</h3>
@@ -60,13 +80,13 @@
 {/snippet}
 
 <section id="features" class="px-6 pb-12 pt-8 lg:py-32">
-	<h3 class="text-text-300 mb-8 text-center font-normal lg:pb-12">
+	<h3 class="mb-8 text-center font-normal text-text-300 lg:pb-12">
 		We created an App to help you quit...
 	</h3>
 
 	<div class="flex flex-col gap-5 lg:gap-24">
-		{#each screens as s}
-			{@render preview(s)}
+		{#each screens as s, i}
+			{@render preview(s, i)}
 		{/each}
 	</div>
 </section>

1acd9db1fa3a8095b269356ab303d278c69cd6fe
MeintrupStefan <72495543+MeintrupStefan@users.noreply.github.com>
2024-12-20 00:43:03 +0100

    Revert "Remove code that bugs"
    This reverts commit e9393ea757523eaf6980b1afb5d0fa6b9717c2ab.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,8 @@
 import * as auth from '$lib/server/auth.js';
 import type { Handle } from '@sveltejs/kit';
 
+//await initDb();
+
 const handleAuth: Handle = async ({ event, resolve }) => {
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
 	if (!sessionToken) {

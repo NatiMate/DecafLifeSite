8da075149eb4209838c6c60dc471259475831778
MeintrupStefan <72495543+MeintrupStefan@users.noreply.github.com>
2024-12-22 19:52:45 +0100

    Add property attribute

--- src/routes/components/Seo.svelte
+++ src/routes/components/Seo.svelte
@@ -12,21 +12,21 @@
 
 <svelte:head>
 	<title>{title}</title>
-	<meta name="title" content={title} />
-	<meta name="description" content={description} />
-	<meta name="robots" content="index, follow" />
-	<meta name="keywords" content={keywords} />
-	<meta name="image" content={image} />
-	<meta name="url" content={url} />
+	<meta name="title" property="title" content={title} />
+	<meta name="description" property="description" content={description} />
+	<meta name="robots" property="robots" content="index, follow" />
+	<meta name="keywords" property="keywords" content={keywords} />
+	<meta name="image" property="image" content={image} />
+	<meta name="url" property="url" content={url} />
 
-	<meta name="og:title" content={title} />
-	<meta name="og:description" content={description} />
-	<meta name="og:image" content={image} />
-	<meta name="og:url" content={url} />
+	<meta name="og:title" property="og:title" content={title} />
+	<meta name="og:description" property="og:description" content={description} />
+	<meta name="og:image" property="og:image" content={image} />
+	<meta name="og:url" property="og:url" content={url} />
 
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content={title} />
-	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image" content={image} />
-	<meta name="twitter:url" content={url} />
+	<meta name="twitter:card" property="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" property="twitter:title" content={title} />
+	<meta name="twitter:description" property="twitter:description" content={description} />
+	<meta name="twitter:image" property="twitter:image" content={image} />
+	<meta name="twitter:url" property="twitter:url" content={url} />
 </svelte:head>
